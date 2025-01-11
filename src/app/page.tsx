0c09630b5296889c3cbbb3d3a640725aa384b07d
Florien Niyongabo <waka@192.168.1.88>
2025-01-12 00:33:42 +0200

    TrendAI UI fixex(Footer)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 import Navbar from "@/components/Nav";
 import { Typography } from "@mui/material";
-// import Image from "next/image";
 
 const currentDate = new Date().getFullYear();
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen h-screen w-full">
+    <div className="flex flex-col items-center min-h-screen h-screen w-full">
       <Navbar isDashboard={false} />
-      <main className="flex flex-col gap-8  items-center ">
-        <div className="flex divide-x mx-20 divide-gray-300">
+      <main className="flex flex-col gap-8  items-center h-screen">
+        <div className="flex divide-x mx-20 divide-black h-full">
           <div className="flex flex-col gap-8 p-20">
             <header className="text-center font-semibold underline">Influencer</header>
             <p>
@@ -25,22 +24,8 @@ export default function Home() {
           </div>
         </div>
       </main>
-      <footer className="sticky bottom-0 w-full p-4 text-center">
-        {/* <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://trendai.app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          &copy; {currentDate} TrendAI
-        </a> */}
+      <footer className=" sticky bottom-0 p-4 text-center bg-[#89e81d] my-8 w-11/12 rounded-md">
+      
         <Typography>
           &copy; {currentDate} TrendAI
         </Typography>
